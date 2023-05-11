f743966f3910efb83c0b77ed6ab0c1b79882ef53
Elijah Lawson <ElijahAtWork@gmail.com>
2023-05-11 16:28:37 -0500

    Fixed a paste error

--- src/index.js
+++ src/index.js
@@ -4,45 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 //Saga Imports
 import {takeLatest, put} from 'redux-saga/effects';
-import SagaMiddleware from 'redux-saga';
-import axios from 'axios';
-//Redux Imports
-import {Provider} from 'react-redux';
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
-//Router Imports
-import {BrowserRouter} from 'react-router-dom';
-
-const gallery = ((state=[], action) => {
-    return state;
-})
-
-const favorites = ((state=[], action) => {
-    return state;
-})
-
-const store = createStore(
-    combineReducers({
-
-    }),
-    applyMiddleware(SagaMiddleware, logger)
-)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
-);
-import React from 'react';
-import './index.css'
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
-//Saga Imports
-import {takeLatest, put} from 'redux-saga/effects';
 import createSagaMiddleware from 'redux-saga';
 import axios from 'axios';
 //Redux Imports
